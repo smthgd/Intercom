0e7134f790ba51334fc6e2cab3add8e737690fb9
smthgd <segendler@edu.hse.ru>
2024-06-26 21:13:29 +0500

    Удалены лишние директивы using

--- IntercomProject/IntercomProject/ApplicationsForm.cs
+++ IntercomProject/IntercomProject/ApplicationsForm.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-using MySqlX.XDevAPI;
 using System.Configuration;
 
 namespace IntercomProject
